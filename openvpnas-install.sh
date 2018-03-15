d55885da2a31b80c0eb616ab96e30815fdd178be
leang97 <leang_97@hotmail.com>
2018-03-15 13:11:34 +0800

    Test Work

---
+++ openvpnas-install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+TEXT_RESET='\e[0m'
+TEXT_YELLOW='\e[0;33m'
+TEXT_RED_B='\e[1;31m'
+
+sudo apt-get update
+echo -e $TEXT_YELLOW
+echo 'APT update finished....'
+echo -e $TEXT_RESET
+
+wget http://swupdate.openvpn.org/as/openvpn-as-2.5-Ubuntu16.amd_64.deb
+dpkg -i openvpn-as-2.5-Ubuntu16.amd_64.deb
+
+
+echo 'Editing Password for Username : openvpn'
+passwd openvpn
